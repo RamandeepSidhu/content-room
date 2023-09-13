065fc8565145e52f0e0764a7f365d3fbbf31ac82
RamandeepSidhu <ramandeep@pixlerlab.com>
2023-09-13 18:18:27 +0530

    json and add package buget update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
@@ -10,7 +10,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'disabled'
+    scrollPositionRestoration: 'disabled',
+    preloadingStrategy: PreloadAllModules
+
   })], exports: [RouterModule]
 })
 export class AppRoutingModule { }
